363ba8b788129d6711d1e7d8fc2475f3d2f1307e
軒竹 吳 <jasonwu@webim.com.tw>
2018-11-23 17:41:35 +0800

    First Test for database

---
+++ IFC2JSON/IFC2JSON/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using GeometryGym.Ifc;
+using System.IO;
+using MongoDB.Driver;
+using MongoDB.Bson;
+
+namespace IFC2JSON
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DatabaseIfc dbIfc = new DatabaseIfc(@"C:\Users\101746\Documents\窗.ifc");
+            foreach (var ele in dbIfc.Context.Extract<IfcElement>())
+            {
+                var property = ele.FindProperty("");
+                var newProperty = new IfcPropertySingleValue(dbIfc,"HirukawaID","");
+                ele.AddAggregated(newProperty);
+            }
+            StreamWriter sw = new StreamWriter(@"C:\Users\101746\Documents\窗.ifc.json");
+            sw.Write(dbIfc.JSON);
+            sw.Close();
+        }
+    }
+}
